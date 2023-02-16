0203a0a60a8f26a7a2755ffed1525d72e7e3c58a
aeynd <aaeynd@gmail.com>
2023-02-16 10:11:53 +0700

    update

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// const fs = require("fs");
 const { User, Product, Categories, Roaster } = require("../models");
 
 const createError = require("../utils/create-error");
